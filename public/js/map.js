7e885d85bddb7ecdf9b8f87313dbe03e454db861
ahkallison <ahkallison@gmail.com>
2017-03-25 15:49:22 -0400

    fixed map things

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,6 @@
 function initMap() {
 	var mapProp = {
-    	center: new google.maps.LatLng{-25.363, 131.044},
+    	center: new google.maps.LatLng(-25.363, 131.044),
     	zoom: 5
     };
 	var map = new google.maps.Map(document.getElementById("gMap"), mapProp);
