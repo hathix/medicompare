265f649e452306ae66d29533387e503c504398d8
Neel Mehta <neelmehta@college.harvard.edu>
2017-03-25 12:29:01 -0400

    start finding nearby zips

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
+  debug: true,
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var zipcodes = require('zipcodes');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'My app' });
 });
 
 module.exports = router;
+
+
+
+/**
+ * Queries SQLite to find procedure cost data near a given location.
+ * @param  {string} procedure   The name of a procedure; see the list in `inpatient-costs.csv`.
+ * @param  {string} zip     Make sure it's 5 digits long ("02138" instead of "2138")
+ * @param  {int} radius     in miles
+ * @return {Object[]}       a list of procedure cost data (as read from 'inpatient-costs.csv'.)
+ */
+function findNearbyProcedureData(procedure, zip, radius) {
+    // find zip codes nearby
+    nearbyZips = zipcodes.radius(zip, radius);
+}
